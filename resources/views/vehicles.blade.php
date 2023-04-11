879f6a484c051f0a1982eed661fc8e18a19265ea
IonSibRe <ionsibre@gmail.com>
2023-04-11 21:29:34 +0200

    added css for bus section in vehicles page

--- resources/views/vehicles.blade.php
+++ resources/views/vehicles.blade.php
@@ -1,17 +1,17 @@
 <x-layout>
     <header class="veh-header">
-        <div class="veh-header-inner">
-            <div class="veh-content">
-                <h1 class="veh-content-title">Vozový <br> <span>park</span></h1>
-                <div class="veh-content-row"></div>
-                <p class="veh-content-text">
-                    Naše autobusy splňují emisní limity EURO 6, čímž umožňujeme klientům levněji
-                    cestovat do zemí, kde jsou tyto normy nutnou podmínkou pro vjezd do některých měst bez dodatkových
-                    poplatků jako například Německo nebo Anglie.
-                </p>
-            </div>
+        <div class="veh-header-content">
+            <h1 class="veh-header-title">VOZOVÝ <br> <span>PARK</span></h1>
+            <div class="veh-header-row"></div>
+            <p class="veh-header-text">
+                Naše autobusy splňují emisní limity EURO 6, čímž umožňujeme klientům levněji
+                cestovat do zemí, kde jsou tyto normy nutnou podmínkou pro vjezd do některých měst bez dodatkových
+                poplatků jako například Německo nebo Anglie.
+            </p>
+
         </div>
-        <img src="{{" imgs/vehicles/veh-header-wave.svg"}}" alt="Vehicles Header Wave" class="veh-header-wave">
+        {{-- <img src="{{" imgs/vehicles/veh-header-wave.svg"}}" alt="Vehicles Header Wave" class="veh-header-wave">
+        --}}
     </header>
     <section class="veh-cards-wrap">
         <div class="veh-cards-container">
@@ -64,103 +64,119 @@
             <div class="veh-buses-row"></div>
             <div class="veh-buses-upper-wrap">
                 <div class="veh-buses-upper-text-wrap">
-                    <h2 class="veh-buses-upper-title">Náhodný autobus 88</h2>
-                    <div class="veh-buses-upper-row"></div>
-                    <p class="veh-buses-upper-text">
-                        INFORMACE O AUTOBUSU, SKVĚLÝ, PROSTORNÝ, VELKÝ. INFORMACE O AUTOBUSU, SKVĚLÝ, PROSTORNÝ, VELKÝ.
-                        INFORMACE O AUTOBUSU, SKVĚLÝ, PROSTORNÝ, VELKÝ. INFORMACE O AUTOBUSU, SKVĚLÝ, PROSTORNÝ, VELKÝ.
-                        INFORMACE O AUTOBUSU, SKVĚLÝ, PROSTORNÝ, VELKÝ. INFORMACE O AUTOBUSU, SKVĚLÝ, PROSTORNÝ, VELKÝ.
-                        INFORMACE O AUTOBUSU, SKVĚLÝ, PROSTORNÝ, VELKÝ.
-                    </p>
+                    <div class="veh-buses-upper-text-inner-wrap">
+                        <h2 class="veh-buses-upper-title">Náhodný autobus 88</h2>
+                        <div class="veh-buses-upper-row"></div>
+                        <p class="veh-buses-upper-text">
+                            INFORMACE O AUTOBUSU, SKVĚLÝ, PROSTORNÝ, VELKÝ. INFORMACE O AUTOBUSU, SKVĚLÝ, PROSTORNÝ,
+                            VELKÝ.
+                            INFORMACE O AUTOBUSU, SKVĚLÝ, PROSTORNÝ, VELKÝ. INFORMACE O AUTOBUSU, SKVĚLÝ, PROSTORNÝ,
+                            VELKÝ.
+                            INFORMACE O AUTOBUSU, SKVĚLÝ, PROSTORNÝ, VELKÝ. INFORMACE O AUTOBUSU, SKVĚLÝ, PROSTORNÝ,
+                            VELKÝ.
+                            INFORMACE O AUTOBUSU, SKVĚLÝ, PROSTORNÝ, VELKÝ.
+                        </p>
+                    </div>
                 </div>
                 <div class="veh-buses-upper-img-wrap">
-                    <img src="{{" imgs/veh-buses-img.jpg"}}" alt="Vehicles Bus Image" class="veh-buses-upper-img">
+                    <img src="{{" imgs/vehicles/veh-buses-img.jpg"}}" alt="Vehicles Bus Image"
+                        class="veh-buses-upper-img">
                 </div>
             </div>
             <div class="veh-buses-lower-wrap">
                 <div class="veh-buses-lower-text-wrap">
                     <h3 class="veh-buses-lower-title">Vybavení</h3>
-                    <div class="veh-buses-lower-icons-wrap">
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-bus"></i>
-                            luxusní daálkový autobus
-                        </p>
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-user"></i>
-                            49 míst k sezení
-                        </p>
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-leaf"></i>
-                            EURO 5
-                        </p>
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-chair"></i>
-                            polohovatelné sedačky
-                        </p>
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-tv"></i>
-                            2x TV
-                        </p>
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-wifi"></i>
-                            WiFi
-                        </p>
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-mug-saucer"></i>
-                            kávovar
-                        </p>
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-font-awesome"></i>
-                            lednice
-                        </p>
-                    </div>
-                    <div class="veh-buses-lower-icons-wrap">
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-shield-halved"></i>
-                            moderní bezpečnostní prvky
-                        </p>
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-snowflake"></i>
-                            klimatizace
-                        </p>
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-location-crosshairs"></i>
-                            GPS
-                        </p>
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-compact-disc"></i>
-                            DVD
-                        </p>
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-microphone"></i>
-                            mikrofon
-                        </p>
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-brands fa-usb"></i>
-                            USB na všech sedadlech
-                        </p>
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-toilet-paper"></i>
-                            toaleta
-                        </p>
-                        <p class="veh-buses-lower-icon-text">
-                            <i class="fa-solid fa-caravan"></i>
-                            tažné zařízení
-                        </p>
+                    <div class="veh-buses-lower-text-inner-wrap">
+                        <div class="veh-buses-lower-icons-wrap">
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-bus"></i>
+                                luxusní dálkový autobus
+                            </p>
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-user"></i>
+                                49 míst k sezení
+                            </p>
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-leaf"></i>
+                                EURO 5
+                            </p>
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-chair"></i>
+                                polohovatelné sedačky
+                            </p>
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-tv"></i>
+                                2x TV
+                            </p>
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-wifi"></i>
+                                WiFi
+                            </p>
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-mug-saucer"></i>
+                                kávovar
+                            </p>
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-font-awesome"></i>
+                                lednice
+                            </p>
+                        </div>
+                        <div class="veh-buses-lower-icons-wrap">
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-shield-halved"></i>
+                                moderní bezpečnostní prvky
+                            </p>
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-snowflake"></i>
+                                klimatizace
+                            </p>
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-location-crosshairs"></i>
+                                GPS
+                            </p>
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-compact-disc"></i>
+                                DVD
+                            </p>
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-microphone"></i>
+                                mikrofon
+                            </p>
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-brands fa-usb"></i>
+                                USB na všech sedadlech
+                            </p>
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-toilet-paper"></i>
+                                toaleta
+                            </p>
+                            <p class="veh-buses-lower-icon-text">
+                                <i class="fa-solid fa-caravan"></i>
+                                tažné zařízení
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div class="veh-buses-lower-imgs-wrap">
                     <div class="veh-buses-lower-imgs-inner-wrap">
-                        <img src="{{" imgs/vehicles/veh-buses-img.jpg"}}" alt="Vehicle Bus Small Image"
-                            class="veh-buses-lower-img">
-                        <img src="{{" imgs/vehicles/veh-buses-img.jpg"}}" alt="Vehicle Bus Small Image"
-                            class="veh-buses-lower-img">
+                        <div>
+                            <img src="{{" imgs/vehicles/veh-cards-img.png"}}" alt="Vehicle Bus Small Image"
+                                class="veh-buses-lower-img">
+                        </div>
+                        <div>
+                            <img src="{{" imgs/vehicles/veh-cards-img.png"}}" alt="Vehicle Bus Small Image"
+                                class="veh-buses-lower-img">
+                        </div>
                     </div>
                     <div class="veh-buses-lower-imgs-inner-wrap">
-                        <img src="{{" imgs/vehicles/veh-buses-img.jpg"}}" alt="Vehicle Bus Small Image"
-                            class="veh-buses-lower-img">
-                        <img src="{{" imgs/vehicles/veh-buses-img.jpg"}}" alt="Vehicle Bus Small Image"
-                            class="veh-buses-lower-img">
+                        <div>
+                            <img src="{{" imgs/vehicles/veh-cards-img.png"}}" alt="Vehicle Bus Small Image"
+                                class="veh-buses-lower-img">
+                        </div>
+                        <div>
+                            <img src="{{" imgs/vehicles/veh-cards-img.png"}}" alt="Vehicle Bus Small Image"
+                                class="veh-buses-lower-img">
+                        </div>
                     </div>
                 </div>
             </div>
